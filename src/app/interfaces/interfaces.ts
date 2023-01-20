8fc9d861cc08693571c9f9bdfd22bf7cc5fba76e
unknown <dankinyi99@gmail.com>
2023-01-20 12:24:43 +0300

    added api service and refactored products to use services

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,5 @@
+export interface services{
+    image:string;
+    title: string;
+    description: string;
+}
--- src/app/product-category/product-category.component.ts
+++ src/app/product-category/product-category.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-interface services{
-  image:string;
-  title: string;
-  description: string;
-}
+import { services } from '../interfaces/interfaces';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-product-category',
@@ -15,13 +11,16 @@ interface services{
 export class ProductCategoryComponent implements OnInit {
   categoryRoute!:string;
 
-services: services[]= [{image:'https://images.pexels.com/photos/442151/pexels-photo-442151.jpeg?auto=compress&cs=tinysrgb&w=600',title:'Dark Lite Fiber',description:'With out dark Fiber, You get Point-to-Point unmonitored Fiber connection tha has virtually unlimited capacity. The product allows you to bring your device to light up'},{image:'https://images.pexels.com/photos/442151/pexels-photo-442151.jpeg?auto=compress&cs=tinysrgb&w=600',title:'Leased Capacity',description:'Leased capacity gives you a dedicated Fiber link that interconnects two or more sites/locations. Our robust metro-Fiber network ensures maximum uptime, allowing for high level efficiency and productivity. With our leased capacity, you will have a protected connection with a minimum of 100mbps dedicated capacity'}];
 
-cloudservices: services[]= [{image:'https://media.istockphoto.com/id/943067460/photo/male-it-specialist-holds-laptop-and-discusses-work-with-female-server-technician-theyre.jpg?b=1&s=612x612&w=0&k=20&c=20Z5WDTeidmPjbSBQee73woV4j3HE9atp8FJ9h8wFGE=',title:'Dark Lite Fiber',description:'With out dark Fiber, You get Point-to-Point unmonitored Fiber connection tha has virtually unlimited capacity. The product allows you to bring your device to light up'},{image:'https://media.istockphoto.com/id/943067460/photo/male-it-specialist-holds-laptop-and-discusses-work-with-female-server-technician-theyre.jpg?b=1&s=612x612&w=0&k=20&c=20Z5WDTeidmPjbSBQee73woV4j3HE9atp8FJ9h8wFGE=',title:'Leased Capacity',description:'Leased capacity gives you a dedicated Fiber link that interconnects two or more sites/locations. Our robust metro-Fiber network ensures maximum uptime, allowing for high level efficiency and productivity. With our leased capacity, you will have a protected connection with a minimum of 100mbps dedicated capacity'}];
-  constructor(private router:Router) { }
+
+services!:services[];
+cloudservices!:services[];
+  constructor(private router:Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.categoryRoute=this.router.url;
+    this.services=this.apiService.getCloudServices();
+    this.cloudservices=this.apiService.getFiberServices();
   }
 
 }
