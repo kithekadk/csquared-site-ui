3c8af27a16579a9ae5cc473d0ee7dcb31d04f907
unknown <dankinyi99@gmail.com>
2023-01-23 16:14:16 +0300

    added csquared images to assets dir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AboutComponent } from './about/about.component'
 import { ProductsComponent } from './products/products.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { SingleServiceComponent } from './single-service/single-service.component';
-<<<<<<< HEAD
 import { CountryHeaderComponent } from './components/country-header/country-header.component';
 import { DRCComponent } from './components/drc/drc.component';
 import { UgandaComponent } from './uganda/uganda.component';
@@ -13,11 +12,8 @@ import { GhanaComponent } from './ghana/ghana.component';
 import { LiberiaComponent } from './liberia/liberia.component';
 import { TogoComponent } from './togo/togo.component';
 import { KenyaComponent } from './kenya/kenya.component';
-
-=======
 import { TeamComponent } from './team/team.component';
 import { BlogComponent } from './blog/blog.component';
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -25,21 +21,20 @@ const routes: Routes = [
     {path: 'team', component: AboutComponent},
     {path: 'team/:id', component: TeamComponent},
     {path: 'services', component: ProductsComponent},
+    {path: 'services/-', component: ProductsComponent},
     {path: 'services/wholesale-fiber', component:ProductCategoryComponent},
     {path: 'services/cloud-solutions', component:ProductCategoryComponent},
     {path:'services/wholesale-fiber/view/:id', component: SingleServiceComponent},
     {path:'services/cloud-solutions/view/:id', component: SingleServiceComponent},
-<<<<<<< HEAD
     // {path:"country/:code", component:CountryHeaderComponent},
     {path:"country/drc", component:DRCComponent},
     {path:"country/uganda", component:UgandaComponent},
     {path:"country/ghana", component:GhanaComponent},
     {path:"country/liberia", component:LiberiaComponent},
     {path:"country/togo", component:TogoComponent},
-    {path:"country/kenya", component:KenyaComponent}
-=======
+    {path:"country/kenya", component:KenyaComponent},
+    {path:"country/hybrid", component:HomeComponent},
     {path: 'blog', component: BlogComponent}
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgOptimizedImage } from '@angular/common'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -17,11 +16,9 @@ import { ProductsComponent } from './products/products.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { SingleServiceComponent } from './single-service/single-service.component';
-<<<<<<< HEAD
 import { CountryHeaderComponent } from './components/country-header/country-header.component';
 import { CountryCoverageComponent } from './components/country-coverage/country-coverage.component';
 import { CountryAboutComponent } from './components/country-about/country-about.component';
-
 import { CountryLatestnewsComponent } from './components/country-latestnews/country-latestnews.component';
 import { WhatWeOfferFlexComponent } from './components/what-we-offer-flex/what-we-offer-flex.component';
 import { WhatWeOfferGridComponent } from './components/what-we-offer-grid/what-we-offer-grid.component';
@@ -31,10 +28,9 @@ import { GhanaComponent } from './ghana/ghana.component';
 import { LiberiaComponent } from './liberia/liberia.component';
 import { TogoComponent } from './togo/togo.component';
 import { KenyaComponent } from './kenya/kenya.component';
-=======
 import { TeamComponent } from './team/team.component';
 import { BlogComponent } from './blog/blog.component';
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
+
 
 @NgModule({
   declarations: [
@@ -51,7 +47,6 @@ import { BlogComponent } from './blog/blog.component';
     NavbarComponent,
     ProductCategoryComponent,
     SingleServiceComponent,
-<<<<<<< HEAD
     CountryHeaderComponent,
     CountryCoverageComponent,
     CountryAboutComponent,
@@ -63,16 +58,15 @@ import { BlogComponent } from './blog/blog.component';
     GhanaComponent,
     LiberiaComponent,
     TogoComponent,
-    KenyaComponent
-=======
+    KenyaComponent,
     TeamComponent,
     BlogComponent
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
