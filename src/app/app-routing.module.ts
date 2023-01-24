d0c5146862db20b3acd16a72afc0a1dacd3e3177
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-01-24 13:31:35 +0300

    Some Changes on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AboutComponent } from './about/about.component'
 import { ProductsComponent } from './products/products.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { SingleServiceComponent } from './single-service/single-service.component';
-<<<<<<< HEAD
 import { CountryHeaderComponent } from './components/country-header/country-header.component';
 import { DRCComponent } from './components/drc/drc.component';
 import { UgandaComponent } from './uganda/uganda.component';
@@ -13,12 +12,8 @@ import { GhanaComponent } from './ghana/ghana.component';
 import { LiberiaComponent } from './liberia/liberia.component';
 import { TogoComponent } from './togo/togo.component';
 import { KenyaComponent } from './kenya/kenya.component';
-
-=======
 import { TeamComponent } from './team/team.component';
 import { BlogComponent } from './blog/blog.component';
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
-
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
@@ -29,17 +24,16 @@ const routes: Routes = [
     {path: 'services/cloud-solutions', component:ProductCategoryComponent},
     {path:'services/wholesale-fiber/view/:id', component: SingleServiceComponent},
     {path:'services/cloud-solutions/view/:id', component: SingleServiceComponent},
-<<<<<<< HEAD
+
     // {path:"country/:code", component:CountryHeaderComponent},
     {path:"country/drc", component:DRCComponent},
     {path:"country/uganda", component:UgandaComponent},
     {path:"country/ghana", component:GhanaComponent},
     {path:"country/liberia", component:LiberiaComponent},
     {path:"country/togo", component:TogoComponent},
-    {path:"country/kenya", component:KenyaComponent}
-=======
+    {path:"country/kenya", component:KenyaComponent},
+
     {path: 'blog', component: BlogComponent}
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,9 @@ import { ProductsComponent } from './products/products.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { SingleServiceComponent } from './single-service/single-service.component';
-<<<<<<< HEAD
 import { CountryHeaderComponent } from './components/country-header/country-header.component';
 import { CountryCoverageComponent } from './components/country-coverage/country-coverage.component';
 import { CountryAboutComponent } from './components/country-about/country-about.component';
-
 import { CountryLatestnewsComponent } from './components/country-latestnews/country-latestnews.component';
 import { WhatWeOfferFlexComponent } from './components/what-we-offer-flex/what-we-offer-flex.component';
 import { WhatWeOfferGridComponent } from './components/what-we-offer-grid/what-we-offer-grid.component';
@@ -31,10 +29,9 @@ import { GhanaComponent } from './ghana/ghana.component';
 import { LiberiaComponent } from './liberia/liberia.component';
 import { TogoComponent } from './togo/togo.component';
 import { KenyaComponent } from './kenya/kenya.component';
-=======
 import { TeamComponent } from './team/team.component';
 import { BlogComponent } from './blog/blog.component';
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
+
 
 @NgModule({
   declarations: [
@@ -51,7 +48,6 @@ import { BlogComponent } from './blog/blog.component';
     NavbarComponent,
     ProductCategoryComponent,
     SingleServiceComponent,
-<<<<<<< HEAD
     CountryHeaderComponent,
     CountryCoverageComponent,
     CountryAboutComponent,
@@ -63,11 +59,9 @@ import { BlogComponent } from './blog/blog.component';
     GhanaComponent,
     LiberiaComponent,
     TogoComponent,
-    KenyaComponent
-=======
+    KenyaComponent,
     TeamComponent,
     BlogComponent
->>>>>>> 4d194269726595658fd93cdd83525ef97fb4ba6d
   ],
   imports: [
     BrowserModule,
