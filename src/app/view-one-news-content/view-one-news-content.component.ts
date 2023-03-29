ce97c2708300cdf8616467809d698aab2ea45fda
unknown <dankinyi99@gmail.com>
2023-03-29 11:08:35 +0300

    working on more news

--- src/app/view-one-news-content/view-one-news-content.component.ts
+++ src/app/view-one-news-content/view-one-news-content.component.ts
@@ -14,6 +14,8 @@ news:any[]=[]
 
 impactTitles:any[]=[]
 impactSlugs:any[]=[]
+impactImages:any[]=[]
+created_at:any[]=[]
 mergedArray:any[]=[]
 
 
@@ -29,16 +31,28 @@ spinneroff=true
 
     this.apiService.getNews().subscribe(res=>{
       console.log(res['data']);
+      // GETTING IMPACT IMAGES
+      for(let image of res['data']){
+        let img = image.media?.pathUrls[0]
+        let avatar = image.name[0]
+        if(img == null || undefined){
+          this.impactImages.push(avatar)
+        }else{
+        this.impactImages.push(img)
+        }                  
+      }
       
       for(let title of res['data']){
         let titles = title.name
         let slug = title.slug
-        // console.log(slug);
+        let time = title.created_at
+        this.created_at.push(new Date(time).toDateString())
+        
         this.impactTitles.push(titles)
         this.impactSlugs.push(slug)
 
         this.mergedArray = this.impactTitles.map((title, index) =>({
-          title:title, slug:this.impactSlugs[index]
+          title:title, slug:this.impactSlugs[index], created_at:this.created_at[index], image:this.impactImages[index]
         }),this.spinneroff=false)
       }
     })
